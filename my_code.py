5fa5ab2955bf6edc1fa869458e38d158a3c71ac6
Itzhak Estrella <itzhak0estrella@gmail.com>
2025-03-26 21:22:20 -0500

    added failing test for invalid inputs

--- my_code.py
+++ my_code.py
@@ -12,3 +12,16 @@ def fix_phone_num(phone_num_to_fix):
 
 def test_fix_phone_num():
   assert fix_phone_num("5125558823") == '(512) 555 8823'
+  # step 1: 
+  assert fix_phone_num('5554429876') == '(555) 442 9876'
+  assert fix_phone_num('3216543333') == '(321) 654 3333'
+
+# step 2: this test is expected to fail
+def test_fix_phone_num_with_bad_formatting():
+  assert fix_phone_num('555-442-98761') == '(555) 442 9876'
+  assert fix_phone_num('(321) 654 3333') == '(321) 654 3333'
+
+# step 3:
+def test_fix_phone_num_raises_value_error():
+  with pytest.raises(ValueError):
+    fix_phone_num('51')
